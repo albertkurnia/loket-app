8a1b6fa07081ac1f21917c4c4b99d134ad3be54c
albertkurnia <albert.kurniawan@amartha.com>
2019-11-12 17:43:40 +0700

    register interface event query

--- modules/event/query/query.go
+++ modules/event/query/query.go
@@ -2,6 +2,7 @@ package query
 
 import (
 	"database/sql"
+	"loket-app/modules/event/model"
 )
 
 type eventQueryImpl struct {
@@ -17,4 +18,10 @@ func NewEventQuery(dbWrite, dbRead *sql.DB) EventQuery {
 }
 
 type EventQuery interface {
+	InsertTicket(data *model.CreateTicketReq) (*model.Ticket, error)
+	LoadTicketByIDs(ids []uint64) ([]*model.Ticket, error)
+	LoadTicketByType(ticketType string) (*model.Ticket, error)
+	InsertEvent(data *model.CreateEventReq) (*model.Event, error)
+	LoadEventByID(id uint64) (*model.Event, error)
+	GetTicket(ticketID uint64) (*model.Ticket, error)
 }
