4259b67a671e214d8b379e1affe4cd0e1a49a54d
albertkurnia <albert.kurniawan@amartha.com>
2019-11-12 17:43:07 +0700

    fix http presenter for event module

--- modules/event/presenter/http_presenter.go
+++ modules/event/presenter/http_presenter.go
@@ -1,9 +1,17 @@
 package presenter
 
 import (
+	"errors"
+	"fmt"
+	"loket-app/helper"
+	"loket-app/modules/event/model"
+	"loket-app/modules/event/sanitizer"
 	"loket-app/modules/event/usecase"
+	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
 )
 
 type eventServiceHTTPHandler struct {
@@ -23,13 +31,77 @@ func (h *eventServiceHTTPHandler) Mount(group *echo.Group) {
 }
 
 func (h *eventServiceHTTPHandler) CreateEvent(c echo.Context) error {
-	return nil
+	logCtx := fmt.Sprintf("%T.CreateEvent", *h)
+
+	params := model.CreateEventReq{}
+	if err := c.Bind(&params); err != nil {
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_bind_params")
+		return helper.NewResponse(http.StatusBadRequest, http.StatusBadRequest, err.Error(), nil).WriteResponse(c)
+	}
+
+	if err := sanitizer.ValidateCreateEvent(&params); err != nil {
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_validate_params")
+		return helper.NewResponse(http.StatusBadRequest, http.StatusBadRequest, err.Error(), nil).WriteResponse(c)
+	}
+
+	resp, err := h.eventUseCase.CreateEvent(&params)
+	if err != nil {
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_create_event")
+		return helper.NewResponse(http.StatusInternalServerError, http.StatusInternalServerError, err.Error(), nil).WriteResponse(c)
+	}
+
+	data := make(map[string]interface{})
+	data["event"] = resp
+	return helper.NewResponse(http.StatusCreated, http.StatusCreated, "Success", data).WriteResponse(c)
 }
 
 func (h *eventServiceHTTPHandler) CreateTicket(c echo.Context) error {
-	return nil
+	logCtx := fmt.Sprintf("%T.CreateTicket", *h)
+
+	params := model.CreateTicketReq{}
+	if err := c.Bind(&params); err != nil {
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_bind_params")
+		return helper.NewResponse(http.StatusBadRequest, http.StatusBadRequest, err.Error(), nil).WriteResponse(c)
+	}
+
+	if err := sanitizer.ValidateCreateTicket(&params); err != nil {
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_validate_params")
+		return helper.NewResponse(http.StatusBadRequest, http.StatusBadRequest, err.Error(), nil).WriteResponse(c)
+	}
+
+	resp, err := h.eventUseCase.CreateTicket(&params)
+	if err != nil {
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_create_ticket")
+		return helper.NewResponse(http.StatusInternalServerError, http.StatusInternalServerError, err.Error(), nil).WriteResponse(c)
+	}
+
+	data := make(map[string]interface{})
+	data["ticket"] = resp
+	return helper.NewResponse(http.StatusCreated, http.StatusCreated, "Success", data).WriteResponse(c)
 }
 
 func (h *eventServiceHTTPHandler) GetEventInfo(c echo.Context) error {
-	return nil
+	logCtx := fmt.Sprintf("%T.GetEventInfo", *h)
+
+	eventID, err := strconv.ParseUint(c.QueryParam("id"), 10, 64)
+	if err != nil {
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_parse_uint")
+		return helper.NewResponse(http.StatusInternalServerError, http.StatusInternalServerError, err.Error(), nil).WriteResponse(c)
+	}
+
+	if eventID <= 0 {
+		err := errors.New("invalid event id")
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_event_id")
+		return helper.NewResponse(http.StatusBadRequest, http.StatusBadRequest, err.Error(), nil).WriteResponse(c)
+	}
+
+	resp, err := h.eventUseCase.GetEventInformation(eventID)
+	if err != nil {
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_get_event_information")
+		return helper.NewResponse(http.StatusBadRequest, http.StatusBadRequest, err.Error(), nil).WriteResponse(c)
+	}
+
+	data := make(map[string]interface{})
+	data["event"] = resp
+	return helper.NewResponse(http.StatusOK, http.StatusOK, "Success", data).WriteResponse(c)
 }
