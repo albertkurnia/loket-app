bb42634b443cd7ea379b2f173399c0792ffdf266
albertkurnia <albert.kurniawan@amartha.com>
2019-11-12 17:46:29 +0700

    sanitizer for transaction module

---
+++ modules/transaction/sanitizer/sanitizer.go
@@ -0,0 +1,31 @@
+package sanitizer
+
+import (
+	"errors"
+	"loket-app/modules/transaction/model"
+)
+
+func ValidatePurchaseTicketRequest(payload *model.PurchaseTicketReq) error {
+
+	if payload == nil {
+		err := errors.New("invalid request")
+		return err
+	}
+
+	if payload.EventID <= 0 {
+		err := errors.New("invalid event id")
+		return err
+	}
+
+	if payload.CustomerID <= 0 {
+		err := errors.New("invalid customer id")
+		return err
+	}
+
+	if len(payload.Ticket) == 0 {
+		err := errors.New("invalid ticket request")
+		return err
+	}
+
+	return nil
+}
