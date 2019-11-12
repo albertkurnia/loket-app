0ccee970f6b543bede8174c4bec08c8141f066de
albertkurnia <albert.kurniawan@amartha.com>
2019-11-12 17:46:04 +0700

    fix http presenter for transaction module

--- modules/transaction/presenter/http_presenter.go
+++ modules/transaction/presenter/http_presenter.go
@@ -1,9 +1,17 @@
 package presenter
 
 import (
+	"errors"
+	"fmt"
+	"loket-app/helper"
+	"loket-app/modules/transaction/model"
+	"loket-app/modules/transaction/sanitizer"
 	"loket-app/modules/transaction/usecase"
+	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
 )
 
 type transactionServiceHTTPHandler struct {
@@ -22,9 +30,52 @@ func (h *transactionServiceHTTPHandler) Mount(group *echo.Group) {
 }
 
 func (h *transactionServiceHTTPHandler) PurchaseTicket(c echo.Context) error {
-	return nil
+	logCtx := fmt.Sprintf("%T.PurchaseTicket", *h)
+
+	params := model.PurchaseTicketReq{}
+	if err := c.Bind(&params); err != nil {
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_bind_params")
+		return helper.NewResponse(http.StatusBadRequest, http.StatusBadRequest, err.Error(), nil).WriteResponse(c)
+	}
+
+	if err := sanitizer.ValidatePurchaseTicketRequest(&params); err != nil {
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_validate_purchase_ticket")
+		return helper.NewResponse(http.StatusBadRequest, http.StatusBadRequest, err.Error(), nil).WriteResponse(c)
+	}
+
+	resp, err := h.transactionUseCase.PurchaseTicket(&params)
+	if err != nil {
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_purchase_ticket")
+		return helper.NewResponse(http.StatusInternalServerError, http.StatusInternalServerError, err.Error(), nil).WriteResponse(c)
+	}
+
+	data := make(map[string]interface{})
+	data["transactionId"] = resp
+	return helper.NewResponse(http.StatusOK, http.StatusOK, "Success", data).WriteResponse(c)
 }
 
 func (h *transactionServiceHTTPHandler) GetTransactionDetail(c echo.Context) error {
-	return nil
+	logCtx := fmt.Sprintf("%T.GetTransactionDetail", *h)
+
+	txID, err := strconv.ParseUint(c.QueryParam("id"), 10, 64)
+	if err != nil {
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_parse_uint")
+		return helper.NewResponse(http.StatusInternalServerError, http.StatusInternalServerError, err.Error(), nil).WriteResponse(c)
+	}
+
+	if txID <= 0 {
+		err := errors.New("invalid transaction id")
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_tx_id")
+		return helper.NewResponse(http.StatusBadRequest, http.StatusBadRequest, err.Error(), nil).WriteResponse(c)
+	}
+
+	resp, err := h.transactionUseCase.GetTransactionDetail(txID)
+	if err != nil {
+		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_get_tx_information")
+		return helper.NewResponse(http.StatusBadRequest, http.StatusBadRequest, err.Error(), nil).WriteResponse(c)
+	}
+
+	data := make(map[string]interface{})
+	data["transaction"] = resp
+	return helper.NewResponse(http.StatusOK, http.StatusOK, "Success", data).WriteResponse(c)
 }
