d00ccabd56ae76161a7bfe9da17fbcf3f5032aa2
albertkurnia <albert.kurniawan@amartha.com>
2019-11-12 17:44:23 +0700

    location model

--- modules/location/model/model.go
+++ modules/location/model/model.go
@@ -1,2 +1,25 @@
 package model
 
+import "time"
+
+type (
+	Location struct {
+		ID       uint64 `json:"id"`
+		Name     string `json:"name"`
+		Address  string `json:"address"`
+		Province string `json:"province"`
+		BaseTime
+	}
+
+	CreateLocationReq struct {
+		Name     string `json:"name"`
+		Address  string `json:"address"`
+		Province string `json:"province"`
+	}
+
+	BaseTime struct {
+		CreatedAt time.Time  `json:"createdAt"`
+		UpdatedAt *time.Time `json:"updatedAt"`
+		DeletedAt *time.Time `json:"deletedAt"`
+	}
+)
