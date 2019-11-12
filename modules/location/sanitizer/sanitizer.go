c95ca15d603146cf6f117d3aa2c721bdc701c4e5
albertkurnia <albert.kurniawan@amartha.com>
2019-11-12 17:45:06 +0700

    sanitizer for location module

---
+++ modules/location/sanitizer/sanitizer.go
@@ -0,0 +1,32 @@
+package sanitizer
+
+import (
+	"errors"
+	"loket-app/modules/location/model"
+	"strings"
+)
+
+func ValidateLocation(payload *model.CreateLocationReq) error {
+
+	if payload == nil {
+		err := errors.New("invalid payload")
+		return err
+	}
+
+	if strings.TrimSpace(payload.Name) == "" {
+		err := errors.New("name is required")
+		return err
+	}
+
+	if strings.TrimSpace(payload.Address) == "" {
+		err := errors.New("address is required")
+		return err
+	}
+
+	if strings.TrimSpace(payload.Province) == "" {
+		err := errors.New("province name is required")
+		return err
+	}
+
+	return nil
+}
