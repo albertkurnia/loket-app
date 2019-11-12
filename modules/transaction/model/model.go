c8f570b82262f2f48307b98a4ffec29388650882
albertkurnia <albert.kurniawan@amartha.com>
2019-11-12 17:45:50 +0700

    init transaction model

--- modules/transaction/model/model.go
+++ modules/transaction/model/model.go
@@ -1,1 +1,30 @@
-packa
+package model
+
+import "time"
+
+type (
+	Transaction struct {
+		ID         uint64      `json:"id"`
+		EventID    uint64      `json:"eventId"`
+		CustomerID uint64      `json:"customerId"`
+		Ticket     []TicketReq `json:"ticket"`
+		BaseTime
+	}
+
+	PurchaseTicketReq struct {
+		EventID    uint64      `json:"eventId"`
+		CustomerID uint64      `json:"customerId"`
+		Ticket     []TicketReq `json:"ticket"`
+	}
+
+	TicketReq struct {
+		TicketID uint64 `json:"ticketId"`
+		Quantity uint64 `json:"qt"`
+	}
+
+	BaseTime struct {
+		CreatedAt time.Time  `json:"createdAt"`
+		UpdatedAt *time.Time `json:"updatedAt"`
+		DeletedAt *time.Time `json:"deletedAt"`
+	}
+)
