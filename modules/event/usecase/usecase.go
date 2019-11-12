c7e6495c1665203849bf42f6f0162d2f1cdff21c
albertkurnia <albert.kurniawan@amartha.com>
2019-11-12 17:44:16 +0700

    register interface event usecase

--- modules/event/usecase/usecase.go
+++ modules/event/usecase/usecase.go
@@ -1,18 +1,26 @@
 package usecase
 
 import (
+	"loket-app/modules/event/model"
 	"loket-app/modules/event/query"
+	locUsecase "loket-app/modules/location/usecase"
 )
 
 type eventUseCaseImpl struct {
-	EventQuery query.EventQuery
+	EventQuery      query.EventQuery
+	LocationUseCase locUsecase.LocationUseCase
 }
 
-func NewEventUseCase(eventQuery query.EventQuery) EventUseCase {
+func NewEventUseCase(eventQuery query.EventQuery, locUC locUsecase.LocationUseCase) EventUseCase {
 	return &eventUseCaseImpl{
-		EventQuery: eventQuery,
+		EventQuery:      eventQuery,
+		LocationUseCase: locUC,
 	}
 }
 
 type EventUseCase interface {
+	CreateTicket(data *model.CreateTicketReq) (*model.Ticket, error)
+	CreateEvent(data *model.CreateEventReq) (*model.Event, error)
+	GetEventInformation(eventID uint64) (*model.EventInformation, error)
+	GetTicket(ticketID uint64) (*model.Ticket, error)
 }
