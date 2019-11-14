0eda54f1c0965ab9d4b283cc6a63f27c74f53f15
albertkurnia <albert.kurniawan@amartha.com>
2019-11-14 11:21:06 +0700

    fix event query postgres

--- modules/event/query/event_query_postgres.go
+++ modules/event/query/event_query_postgres.go
@@ -1,6 +1,8 @@
 package query
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 
 	"loket-app/helper"
@@ -10,6 +12,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// InsertTicket - function for inserting ticket to tickets table.
 func (eq *eventQueryImpl) InsertTicket(data *model.CreateTicketReq) (*model.Ticket, error) {
 	logCtx := fmt.Sprintf("%T.InsertTicket", *eq)
 
@@ -35,9 +38,14 @@ func (eq *eventQueryImpl) InsertTicket(data *model.CreateTicketReq) (*model.Tick
 		return nil, err
 	}
 
+	resp.Type = data.Type
+	resp.Quantity = data.Quantity
+	resp.Price = data.Price
+
 	return &resp, nil
 }
 
+// LoadTicketByType - function for loading data from tickets table by ticket type.
 func (eq *eventQueryImpl) LoadTicketByType(ticketType string) (*model.Ticket, error) {
 	logCtx := fmt.Sprintf("%T.LoadTicketByType", *eq)
 
@@ -59,6 +67,11 @@ func (eq *eventQueryImpl) LoadTicketByType(ticketType string) (*model.Ticket, er
 		&resp.ID, &resp.Type, &resp.Quantity,
 		&resp.Price, &resp.CreatedAt, &resp.UpdatedAt, &resp.DeletedAt,
 	); err != nil {
+		if err == sql.ErrNoRows {
+			err := errors.New("ticket not found")
+			helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_ticket_not_found")
+			return nil, err
+		}
 		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_exec_database")
 		return nil, err
 	}
@@ -66,6 +79,7 @@ func (eq *eventQueryImpl) LoadTicketByType(ticketType string) (*model.Ticket, er
 	return &resp, nil
 }
 
+// InsertEvent - function for inserting event to events table.
 func (eq *eventQueryImpl) InsertEvent(data *model.CreateEventReq) (*model.Event, error) {
 	logCtx := fmt.Sprintf("%T.InsertEvent", *eq)
 
@@ -78,8 +92,7 @@ func (eq *eventQueryImpl) InsertEvent(data *model.CreateEventReq) (*model.Event,
 		"ticketId"
 	) VALUES (
 		$1, $2, $3,
-		$4, $5, %6,
-		$7
+		$4, $5, $6
 	) RETURNING	id, "createdAt"`
 
 	stmt, err := eq.dbWrite.Prepare(sq)
@@ -90,20 +103,30 @@ func (eq *eventQueryImpl) InsertEvent(data *model.CreateEventReq) (*model.Event,
 
 	if err := stmt.QueryRow(
 		data.Title, data.LocationID, data.Description,
-		data.StartDate, data.EndDate,
-		data.TicketID,
+		data.StartDate, data.EndDate, pq.Array(data.TicketID),
 	).Scan(&resp.ID, &resp.CreatedAt); err != nil {
 		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_exec_database")
 		return nil, err
 	}
 
+	resp.Title = data.Title
+	resp.LocationID = data.LocationID
+	resp.Description = data.Description
+	resp.StartDate = data.StartDate
+	resp.EndDate = data.EndDate
+	resp.TicketID = data.TicketID
+
 	return &resp, nil
 }
 
+// LoadEventByID - function for loading data from event tables by event id.
 func (eq *eventQueryImpl) LoadEventByID(id uint64) (*model.Event, error) {
 	logCtx := fmt.Sprintf("%T.LoadEventByID", *eq)
 
-	var resp model.Event
+	var (
+		resp      model.Event
+		ticketIds pq.Int64Array
+	)
 
 	sq := `SELECT id, "title", "locationId", 
 				"description", "startDate", 
@@ -120,16 +143,24 @@ func (eq *eventQueryImpl) LoadEventByID(id uint64) (*model.Event, error) {
 
 	if err := stmt.QueryRow(id).Scan(
 		&resp.ID, &resp.Title, &resp.LocationID,
-		&resp.Description, &resp.StartDate, &resp.EndDate, &resp.TicketID,
-		&resp.DeletedAt, &resp.UpdatedAt, &resp.DeletedAt,
+		&resp.Description, &resp.StartDate, &resp.EndDate, &ticketIds,
+		&resp.CreatedAt, &resp.UpdatedAt, &resp.DeletedAt,
 	); err != nil {
 		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_exec_database")
 		return nil, err
 	}
 
+	temp := []int64(ticketIds)
+	ticketIDs := make([]uint64, 0)
+	for _, i := range temp {
+		ticketIDs = append(ticketIDs, uint64(i))
+	}
+	resp.TicketID = ticketIDs
+
 	return &resp, nil
 }
 
+// LoadTicketByIDs - function for loading tickets data from tickets table by ticket ids.
 func (eq *eventQueryImpl) LoadTicketByIDs(ids []uint64) ([]*model.Ticket, error) {
 	logCtx := fmt.Sprintf("%T.LoadTicketByID", *eq)
 
@@ -174,8 +205,9 @@ func (eq *eventQueryImpl) LoadTicketByIDs(ids []uint64) ([]*model.Ticket, error)
 	return tickets, nil
 }
 
+// GetTicket - function for getting ticket from tickets table by ticket id.
 func (eq *eventQueryImpl) GetTicket(ticketID uint64) (*model.Ticket, error) {
-	logCtx := fmt.Sprintf("%T.GetTicketQuota", *eq)
+	logCtx := fmt.Sprintf("%T.GetTicket", *eq)
 
 	var ticket model.Ticket
 
@@ -196,6 +228,11 @@ func (eq *eventQueryImpl) GetTicket(ticketID uint64) (*model.Ticket, error) {
 		&ticket.Price, &ticket.CreatedAt, &ticket.UpdatedAt,
 		&ticket.DeletedAt,
 	); err != nil {
+		if err == sql.ErrNoRows {
+			err := errors.New("ticket not found")
+			helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_ticket_not_found")
+			return nil, err
+		}
 		helper.Log(logrus.ErrorLevel, err.Error(), logCtx, "error_exec_database")
 		return nil, err
 	}
